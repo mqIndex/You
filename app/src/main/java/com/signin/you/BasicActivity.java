ee4f117459225b821b290f65833f7913426e8362
WDNMD <534290047@qq.com>
2019-07-01 15:48:25 +0800

    项目建立

---
+++ app/src/main/java/com/signin/you/BasicActivity.java
@@ -0,0 +1,45 @@
+package com.signin.you;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import cn.signin.you.ActivtyCollector;
+
+public class BasicActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private static final String TAG = "BasicActivity";
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: "+getClass().getSimpleName());
+        ActivtyCollector.addActivity(this);
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivtyCollector.removeActivity(this);
+    }
+    @Override
+    public void onClick(View view) {
+
+    }
+    public void fullScreen(){
+        //去除标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //去除状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.hide();
+        }
+    }
+}
---
+++ app/src/main/java/com/signin/you/MainActivity.java
@@ -0,0 +1,13 @@
+package com.signin.you;
+
+
+import android.os.Bundle;
+
+public class MainActivity extends BasicActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
